fa6b6e8801d53a70562b29282b2f841f949a6719
aparnabreddy <aparnabreddy26@gmail.com>
2021-07-21 07:17:30 +0530

    list modified

--- list.py
+++ list.py
@@ -1,12 +1,17 @@
 names=["aparna","ajay","impana","darshan","sahana",23,22,21]
+students=["akshay,""aadrika","krupa"]
+states=["Karnataka","Ap","Up"]
 print(names)
+print(len(names))
+print(names+students)#list concatenation
+print(states*3) #replication of list
 # print(names[2])
 # print(names[2:4])
 # del names[3]
 # print(names)
 # names.pop(1)
 # print(names)
-names.remove('impana')
-print(names)
-names.clear()
-print(names)
+# names.remove('impana')
+# print(names)
+# names.clear()
+# print(names)
