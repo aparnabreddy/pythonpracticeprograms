07795ec4b34aa0481708756429fed625a317bf9a
aparnabreddy <aparnabreddy26@gmail.com>
2021-07-21 12:09:04 +0530

    operations on list, tuple and dictionary

---
+++ listmembership.py
@@ -0,0 +1,6 @@
+students=["arif","aadrika","Aparna","amarthiga",23,26,22]
+name="rahul"
+if(name in students):
+    print("he is present")
+else:
+    print("he is absent")
---
+++ tuple.py
@@ -0,0 +1,31 @@
+names=("Aparna","aadrika","siddarth","vaishnavi")
+print(names)
+# names[0]="sita" #tuple is immutable
+# print(names)
+# del names[1] #tuple is immutable
+# del names  # we can delete the entire tuple
+# print(names)
+
+# tuple is immutable, del, pop, clear won't work with tuples.
+
+# print(names)
+# print(len(names)) #finding length of tuple
+numbers=(1,2,3,4,5)
+# print(numbers)
+# print(names+numbers) # tuple concatenation
+# print(len(names+numbers))
+
+print(max(numbers))
+print(min(numbers))
+print(max(names))
+print(min(names))
+
+print(list(names)) #converts tuple to list
+
+# item="aadrika"
+# if(item in names):
+#     print("she is present")
+# else:
+#     print("she is absent")
+
+
